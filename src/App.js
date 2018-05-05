52950d7d3df749c878f8f8e96f1ea8d2095ec351
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-05-05 20:24:06 +1000

    fix to work with empty query results

--- src/App.js
+++ src/App.js
@@ -32,21 +32,26 @@ class BooksApp extends React.Component {
   }
 
   async search(query) {
-    console.log("query: " + query);
+    // console.log("query: " + query);
     let resultsLibrary = [];
     if (query === null || query === "") {
+      //console.log("empty query, flush state");
+      this.flushState();
       this.setState({ searchResults: [] });
-    } else {
+    } 
+    else {
       this.flushState();
       this.setState({ library: await BooksAPI.getAll() });
       await BooksAPI.search(query).then(results => {
         //console.log(results);
-        resultsLibrary = results.map(
+        if (results && results.length>0) {
+          resultsLibrary = results.map(
           result =>
             this.state.library.find(item => item.id === result.id)
               ? this.state.library.find(item => item.id === result.id)
               : result
         );
+      }
         //console.log(resultsLibrary);
         //match results with library, add shelf if match
         this.setState({ currentQuery: query, searchResults: resultsLibrary });
